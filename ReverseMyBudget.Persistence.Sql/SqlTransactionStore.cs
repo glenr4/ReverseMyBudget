69b068ca68a8ce688c42a630699b0b78785c3744
Glen Rutherford <glenrutherford4@gmail.com>
2020-04-11 14:25:26 +1000

    Moved CreatePredicate to extension method

--- ReverseMyBudget.Persistence.Sql/SqlTransactionStore.cs
+++ ReverseMyBudget.Persistence.Sql/SqlTransactionStore.cs
@@ -1,10 +1,5 @@
-﻿using LinqKit;
-using ReverseMyBudget.Domain;
-using System;
+﻿using ReverseMyBudget.Domain;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Dynamic.Core;
-using System.Reflection;
 using System.Threading.Tasks;
 
 namespace ReverseMyBudget.Persistence.Sql
@@ -15,35 +10,12 @@ namespace ReverseMyBudget.Persistence.Sql
         {
         }
 
-        //public Task<PagedList<Transaction>> Get(TransactionQueryParameters parameters)
-        //{
-        //    var predicate = PredicateBuilder.New<Transaction>(true);
-        //    predicate = predicate.And(t => t.Description.Contains(parameters.Description));
-
-        //    var test = new QueryParamsToPredicateBuilder();
-        //    test.ReadEntity(parameters);
-
-        //    return PagedList<Transaction>.ToPagedListAsync(
-        //        QueryAll<Transaction>(),
-        //        predicate,
-        //        parameters.PageNumber,
-        //        parameters.PageSize);
-        //}
-
-        public async Task<PagedList<Transaction>> Get(TransactionQueryParameters parameters)
+        public Task<PagedList<Transaction>> Get(TransactionQueryParameters parameters)
         {
-            //var test = CreatePredicate(QueryAll<Transaction>(), parameters);
-
-            var query = QueryAll<Transaction>();
-
-            var test2 = await PagedList<Transaction>.ToPagedListAsync(
-                //QueryAll<Transaction>().Where("Type == @0", "PURCHASE AUTHORISATION"),
-                //QueryAll<Transaction>().Where("t => t.Type.Contains(@0)", "AUTHORISATION").Where(a => a.AccountId == new Guid("db64ea81-51fe-40d8-aa08-00851184b396")),
-                CreatePredicate(query, parameters),
+            return PagedList<Transaction>.ToPagedListAsync(
+                QueryAll<Transaction>().CreatePredicate(parameters),
                 parameters.PageNumber,
                 parameters.PageSize);
-
-            return test2;
         }
 
         public Task AddAsync(IEnumerable<Transaction> transactions)
@@ -51,42 +23,6 @@ namespace ReverseMyBudget.Persistence.Sql
             _ctx.Transaction.AddRange(transactions);
 
             return _ctx.SaveChangesAsync();
-        }
-
-        public IQueryable<T1> CreatePredicate<T1, T2>(IQueryable<T1> queryable, T2 parameters)
-        {
-            foreach (var property in typeof(T2).GetProperties(BindingFlags.Instance | BindingFlags.Public).Where(p => p.CanWrite))
-            {
-                // string contains
-                if (property.PropertyType == typeof(string))
-                {
-                    var value = property.GetValue(parameters);
-                    if (value != default)   // only query if property has a value
-                    {
-                        var query = $"q => q.{property.Name}.Contains(@0)";
-
-                        queryable = queryable.Where(query, value);
-                    }
-                }
-
-                // >= StartDate  or <= EndDate
-                if (property.PropertyType == typeof(DateRange))
-                {
-                    var value = (DateRange)property.GetValue(parameters);
-                    if (value?.StartDate != default)   // only query if property has a value
-                    {
-                        string query = $"q => q.{property.Name} >= @0";
-                        queryable = queryable.Where(query, value.StartDate);
-                    }
-                    if (value?.EndDate != default)   // only query if property has a value
-                    {
-                        string query = $"q => q.{property.Name} <= @0";
-                        queryable = queryable.Where(query, value.EndDate);
-                    }
-                }
-            }
-
-            return queryable;
         }
     }
 }
--- ReverseMyBudget.Persistence/TransactionQueryParameters.cs
+++ ReverseMyBudget.Persistence/TransactionQueryParameters.cs
@@ -2,16 +2,17 @@
 
 namespace ReverseMyBudget.Persistence
 {
+    /// <summary>
+    /// Note: all parameter names must be the same as the type being filtered
+    /// </summary>
     public class TransactionQueryParameters : PagingQueryParameters
     {
-        public string Description { get; set; }
-        public string Test { get; set; }
         public DateRange DateLocal { get; set; } = new DateRange();
-    }
-
-    public class DateRange
-    {
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
+        public decimal Amount { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public Guid AccountId { get; set; }
+        public Guid? CategoryId { get; set; }
+        public bool IsDuplicate { get; set; }
     }
 }
