9b9a2d0b382c82fa001b15a241571e415c63b131
Glen Rutherford <glenrutherford4@gmail.com>
2020-04-22 18:43:22 +1000

    Moved creating stored procedure into a class

---
+++ ReverseMyBudget.Persistence.Sql/StoredProcedureAddDistinctToTransactions.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReverseMyBudget.Persistence.Sql
+{
+    public class StoredProcedureAddDistinctToTransactions
+    {
+        public void Create(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"-- Inserts all new records from TransactionStaging to Transaction for a User
+-- ie if it already exists in Transaction table then it will be ignored
+-- Used to maintain the data integrity of the Transaction table
+
+CREATE PROCEDURE [dbo].[sp_AddDistinctToTransactions]
+	@UserId uniqueidentifier,
+	@RowCount int Output	-- returns the number of rows inserted
+AS
+BEGIN
+
+INSERT INTO [dbo].[Transaction]
+           ([UserId]
+		   ,[DateLocal]
+           ,[Amount]
+           ,[Type]
+           ,[Description]
+           ,[Balance]
+		   ,[AccountId]
+		   ,[ImportOrder])
+(
+	select [UserId]
+		   ,[DateLocal]
+           ,[Amount]
+           ,[Type]
+           ,[Description]
+           ,[Balance]
+		   ,[AccountId]
+		   ,[ImportOrder]
+	from [dbo].[TransactionStaging] AS ts
+	WHERE UserId = @UserId
+		AND NOT EXISTS
+		(
+			 Select 1
+			 From [Transaction] AS t
+			 Where ts.DateLocal = t.DateLocal
+				 AND ts.Amount = t.Amount
+				 AND ts.Description = t.Description
+				 AND ts.UserId = t.UserId
+		)
+);
+
+-- Return INSERT (only) row count, must be done before DELETE
+Select @RowCount = @@ROWCOUNT;
+
+-- Clear the staging table for the User
+DELETE FROM [dbo].[TransactionStaging]
+WHERE UserId = @UserId;
+
+END
+
+GO
+
+";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        public void Drop(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"DROP PROCEDURE [dbo].[sp_AddDistinctToTransactions]
+GO";
+
+            migrationBuilder.Sql(sp);
+        }
+    }
+}
